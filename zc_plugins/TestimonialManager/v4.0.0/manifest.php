6c3337b4fc080b1a88b14b1d13ca625218e2c0d4
lat9 <cindy@vinosdefrutastropicales.com>
2026-05-06 09:20:53 -0400

    Update plugin version to v4.0.0 from v3.0.0

--- zc_plugins/TestimonialManager/v4.0.0/manifest.php
+++ zc_plugins/TestimonialManager/v4.0.0/manifest.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'pluginVersion' => 'v3.0.0',
+    'pluginVersion' => 'v4.0.0',
     'pluginName' => 'Testimonials Manager',
-    'pluginDescription' => 'This is a Testimonials Manager Module for ZC1.5.8 and up.',
-    'pluginAuthor' => 'CowboyGeek.com (davewest)',
+    'pluginDescription' => 'This is a Testimonials Manager Module for zc222 and later.',
+    'pluginAuthor' => 'CowboyGeek.com (davewest), Vinos de Frutas Tropicales (lat9)',
     'pluginId' => 299, // ID from Zen Cart forum
     'zcVersions' => ['v158'],
     'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
